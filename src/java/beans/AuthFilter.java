8e211b79e6d5ea8c880683440e1e14ebbb1ce5df
LukeGrif <21334528@studentmail.ul.ie>
2025-04-21 11:11:05 +0100

    Updated Security

---
+++ src/java/beans/AuthFilter.java
@@ -0,0 +1,49 @@
+package beans;
+
+import beans.LoginBean;
+import entities.Admin;
+import entities.Freelancer;
+import entities.Provider;
+import jakarta.inject.Inject;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("*.xhtml")
+public class AuthFilter implements Filter {
+
+    @Inject
+    private LoginBean loginBean;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+
+//        if (loginBean == null || loginBean.getLoggedInUser() == null) {
+//            // Not logged in
+//            res.sendRedirect(req.getContextPath() + "/login.xhtml");
+//            return;
+//        }
+
+        // Role-based restriction
+        if (uri.contains("/admin/") && !(loginBean.getLoggedInUser() instanceof Admin)) {
+            res.sendRedirect(req.getContextPath() + "/unauthorised.xhtml");
+            return;
+        } else if (uri.contains("/provider/") && !(loginBean.getLoggedInUser() instanceof Provider)) {
+            res.sendRedirect(req.getContextPath() + "/unauthorised.xhtml");
+            return;
+        } else if (uri.contains("/freelancer/") && !(loginBean.getLoggedInUser() instanceof Freelancer)) {
+            res.sendRedirect(req.getContextPath() + "/unauthorised.xhtml");
+            return;
+        }
+
+        // All good
+        chain.doFilter(request, response);
+    }
+}
--- src/java/startup/UserSeederBean.java
+++ src/java/startup/UserSeederBean.java
@@ -5,7 +5,6 @@ import entities.Freelancer;
 import entities.Provider;
 import jakarta.annotation.PostConstruct;
 import jakarta.ejb.Singleton;
-import jakarta.ejb.Startup;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
